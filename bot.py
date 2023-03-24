76f976a1badb22a7244aace991379c5cf869c608
mastercoms <mastercoms@tuta.io>
2023-03-24 15:02:20 -0400

    more updates (perf, stability, security)

--- bot.py
+++ bot.py
@@ -1,17 +1,35 @@
+from __future__ import annotations
+
 import os
 import string
 
 import discord
+import profanity_check
 
-from discord import Intents
+from discord import Intents, AllowedMentions
 from thefuzz import fuzz
+
+if os.name == "nt":
+    # handle Windows imports
+    # for colored terminal
+    import colorama
+
+    colorama.init()
+else:
+    # handle POSIX imports
+    # for uvloop
+    # while we have steam client, we cannot use uvloop due to gevent
+    import uvloop
+
+    uvloop.install()
+
 
 intents = Intents.none()
 intents.guilds = True
 intents.guild_messages = True
 intents.message_content = True
 
-mentions = discord.AllowedMentions.none()
+mentions = AllowedMentions.none()
 
 client = discord.Client(
     intents=intents,
@@ -19,19 +37,23 @@ client = discord.Client(
 )
 
 THANKING_WORDS = ["thank", "vroom", "zoom", "nyoom"]
+MAX_THANKNESS = float(len(THANKING_WORDS))
 client.thank_channels = set()
 client.thank_pairs = {}
 
 
 @client.event
-async def on_guild_join(guild):
+async def on_guild_join(guild: discord.Guild):
     collect_from_guild(guild)
 
 
-def collect_from_guild(guild):
+def collect_from_guild(guild: discord.Guild):
     thank_channel = discord.utils.find(lambda c: c.name == 'thank', guild.channels)
 
     if thank_channel is None:
+        return
+
+    if not isinstance(thank_channel, discord.TextChannel):
         return
 
     client.thank_channels.add(thank_channel)
@@ -48,7 +70,7 @@ async def on_ready():
 bad_chars = set('/{}\\%$[]#()-=<>|^@`*_')
 
 @client.event
-async def on_message(message):
+async def on_message(message: discord.Message):
     if message.author == client.user or message.author.bot:
         return
 
@@ -80,22 +102,25 @@ async def on_message(message):
     if not text:
         return
 
-    if get_thankness(text, THANKING_WORDS) > 0.7:
+    if profanity_check.predict([text])[0] > 0.5:
+        return
+
+    if get_thankness(text) > 0.7:
         thank_msg = await message.channel.send(text)
         client.thank_pairs[message.guild.id][message.id] = thank_msg
 
 
 @client.event
-async def on_message_delete(message):
+async def on_message_delete(message: discord.Message):
     await delete_from_message(message)
 
 
 @client.event
-async def on_message_edit(before, _after):
+async def on_message_edit(before: discord.Message, _after: discord.Message):
     await delete_from_message(before)
 
 
-async def delete_from_message(message):
+async def delete_from_message(message: discord.Message):
     if message.author == client.user or message.author.bot:
         return
 
@@ -108,22 +133,22 @@ async def delete_from_message(message):
             pass
 
 
-def get_thankness(text, keywords):
-    thankness = 0
-    max_thankness = len(keywords)
+def get_thankness(text: str) -> float:
+    thankness = 0.0
+    max_thankness = MAX_THANKNESS
 
-    for keyword in keywords:
+    for keyword in THANKING_WORDS:
         partial = fuzz.partial_ratio(keyword, text)
         if partial > 70:
             # if the phrase fuzzes a keyword, calculate thankness for that keyword
-            thankness += fuzz.ratio(keyword, text) / 100
+            thankness += fuzz.ratio(keyword, text) / 100.0
         else:
             # if we don't have that phrase, don't consider it for calculating our final thankness ratio
             max_thankness -= 1
 
     # we didn't fuzz any keywords, no thankness
     if max_thankness < 1:
-        return 0
+        return 0.0
 
     return thankness * max_thankness
 
